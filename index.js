69bb1a1cc211e94f849b3d12c7ff62d3ffc309dc
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-31 08:43:25 +0300

    Add User model

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
+mongoose.connect(process.env.MONGODB_URI)
 
 const app = express()
 
