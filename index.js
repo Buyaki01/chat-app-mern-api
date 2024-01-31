abab74f83955e30a3820e994612ff6816491b080
Buyaki01 <rittahbuyaki@gmail.com>
2024-01-31 09:48:00 +0300

    Install cors, generate token when user registers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
 const User = require('./models/User')
 
 dotenv.config()
@@ -9,6 +10,11 @@ mongoose.connect(process.env.MONGODB_URI)
 const jwtSecret = process.env.JWT_SECRET_KEY
 
 const app = express()
+app.use(express.json())
+app.use(cors({
+  credentials: true,
+  origin: process.env.ALLOWED_ORIGINS,
+}))
 
 app.get('/test', (req,res) => {
   res.json('test ok')
@@ -16,12 +22,20 @@ app.get('/test', (req,res) => {
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body
-  const user = await User.create({username, password})
 
-  jwt.sign({name: user.username}, jwtSecret, (err, token) => {
-    if (err) throw err
-    res.cookie('token', token).status(201).json('Ok')
-  })
+  try {
+    const user = await User.create({username, password})
+
+    jwt.sign({name: user.username}, jwtSecret, {}, (err, token) => {
+      if (err) throw err
+      res.cookie('token', token).status(201).json({
+        name: user.username,
+      })
+    })
+  } catch (error) {
+    if (error) throw error
+    res.status(500).json('error')
+  }
 })
 
 app.listen(5000)
--- models/User.js
+++ models/User.js
@@ -8,4 +8,5 @@ const UserSchema = new mongoose.Schema({
   password: String,
 }, { timestamps: true })
 
-export const UserModel = mongoose.model('User', UserSchema)
+const UserModel = mongoose.model('User', UserSchema)
+module.exports = UserModel
